f4dff31c18867fa75ecbf6afc08b2b0258ba316b
Nabarka1999 <82209383+Nabarka1999@users.noreply.github.com>
2025-01-22 16:22:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: "https://dashboard-rox-frontend.vercel.app", 
   methods: ["GET", "POST", "PUT", "DELETE"], 
   allowedHeaders: ["Content-Type", "Authorization"], 
 }));
