ba63dd2d7324940fe13d50e4856fc21d19e649f1
Nabarka1999 <nabarkaroy.20.8@gmail.com>
2025-01-22 16:03:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const initializeRoutes = require("./routes/initializeRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors({
+  origin: "http://localhost:3000", 
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
+}));
+app.use(express.json());
+app.use("/api", initializeRoutes);
+app.use("/api/products", productRoutes);
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+// Define a basic route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
